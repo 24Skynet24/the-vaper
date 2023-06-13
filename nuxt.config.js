1a8716c32e740c25b4a9274c120fec2e09ffd638
albert.mnac <albert.mnac@gmail.com>
2023-06-13 18:15:28 +0400

    fix: base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,7 @@ export default {
 
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.BASE_URL || 'http://194.67.86.108:89',
+    baseURL: process.env.BASE_URL || 'https://the-vaper.itwonder.ru',
     headers: {
       common: {
         'Accept': 'application/json'
